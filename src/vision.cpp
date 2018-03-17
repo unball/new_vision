c6e03c8fed55daa4c796a34aa31e8a1e67d85314
sofia <sofiacatharina@gmail.com>
2018-03-17 16:38:38 -0300

    trying to open webcam fails

--- src/vision.cpp
+++ src/vision.cpp
@@ -4,6 +4,20 @@
 
 int main(int argc, char **argv){
 
+    if (argc < 2){
+        ROS_ERROR("Missing Arguments: aborting");
+        exit(1);
+    }
+
+    cv::VideoCapture raw_input(argv[1]);
+    while (!raw_input.isOpened()) { //check if video device has been initialised
+        ROS_INFO("cannot open camera");
+    }
+
+    raw_input.open(argv[1]);
+    ROS_INFO("%d",raw_input.isOpened());
+    cv::namedWindow("raw_input");
+
     ros::init(argc, argv, "vision_node");
     ros::NodeHandle node_handle;
 
@@ -11,9 +25,15 @@ int main(int argc, char **argv){
     ros::Rate loop_rate(30);
 
     while (ros::ok()){
-        ROS_INFO("running");
+        
+        cv::Mat frame;
+
+        raw_input >> frame;
+        cv::imshow("raw_input", frame);
+
         loop_rate.sleep();
     }
+
 
     return 0;
 }
