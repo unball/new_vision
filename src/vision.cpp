28a016dd0d5544492a1bd7070a8285f12bd6cc12
sofia <sofiacatharina@gmail.com>
2018-03-17 17:29:32 -0300

    image not showing

--- src/vision.cpp
+++ src/vision.cpp
@@ -1,6 +1,24 @@
 #include <ros/ros.h>
 #include <opencv2/opencv.hpp>
 #include <vision/VisionMessage.h>
+#include <image_transport/image_transport.h>
+#include <cv_bridge/cv_bridge.h>
+#include <sensor_msgs/image_encodings.h>
+
+cv::Mat frame;
+
+void save_frame(const sensor_msgs::ImageConstPtr& msg){
+    cv_bridge::CvImagePtr cv_ptr;
+    try{
+        cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
+    }
+    catch (cv_bridge::Exception& e){
+        ROS_ERROR("cv_bridge exception: %s", e.what());
+        return;
+    }
+    cv::imshow("raw_input", cv_ptr->image);
+
+}
 
 int main(int argc, char **argv){
 
@@ -9,28 +27,14 @@ int main(int argc, char **argv){
         exit(1);
     }
 
-    cv::VideoCapture raw_input(argv[1]);
-    while (!raw_input.isOpened()) { //check if video device has been initialised
-        ROS_INFO("cannot open camera");
-    }
-
-    raw_input.open(argv[1]);
-    ROS_INFO("%d",raw_input.isOpened());
-    cv::namedWindow("raw_input");
-
     ros::init(argc, argv, "vision_node");
     ros::NodeHandle node_handle;
-
+    ros::Subscriber sub = node_handle.subscribe("image_raw", 0, save_frame);
     ros::Publisher publisher = node_handle.advertise<vision::VisionMessage>("vision_topic", 1);
     ros::Rate loop_rate(30);
 
     while (ros::ok()){
         
-        cv::Mat frame;
-
-        raw_input >> frame;
-        cv::imshow("raw_input", frame);
-
         loop_rate.sleep();
     }
 
