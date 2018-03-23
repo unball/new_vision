0660e2b66785dc3ccf16af163a2def17241efcfc
Igor Beduin <igor.rob.96@gmail.com>
2018-03-23 11:00:04 -0300

    starting to work with opencv without ros

---
+++ temp/CMakeFiles/new_vision.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/new_vision.dir/src/new_vision.cpp.o"
+  "new_vision.pdb"
+  "new_vision"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/new_vision.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
